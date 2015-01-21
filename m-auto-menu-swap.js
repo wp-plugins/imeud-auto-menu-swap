49ce60b12a14e7c37f3d42b18603add0e82b0f5a
kittichai <kittichai@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-21 06:06:09 +0000

    Compress script
    git-svn-id: https://plugins.svn.wordpress.org/imeud-auto-menu-swap/trunk@1072248 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- m-auto-menu-swap.js
+++ m-auto-menu-swap.js
@@ -1,12 +1,1 @@
-(function($){
-$(document).ready(function(){
-$(window).scroll(function(){
-	if($(this).scrollTop()>100){ 
-		$("nav").addClass("swap_header_footer"); 
-		$("footer").attr('style', 'margin-bottom:'+$("nav").height()+'px !important');
-	}else{
-		$("nav").removeClass("swap_header_footer");
-	}
-})
-})
-})(jQuery);
+!function(o){o(document).ready(function(){o(window).scroll(function(){o(this).scrollTop()>100?(o("nav").addClass("swap_header_footer"),o("footer").attr("style","margin-bottom:"+o("nav").height()+"px !important")):o("nav").removeClass("swap_header_footer")})})}(jQuery);
